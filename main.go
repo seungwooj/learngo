817863cb5f0c34214c33959dfb332b4190809943
seungwooj <seungwoo.aaron@gmail.com>
2021-05-01 13:13:15 +0900

    update GO theory

--- main.go
+++ main.go
@@ -2,33 +2,32 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/seungwooj/learngo/mydict"
+	"time"
 )
 
 func main() {
-	dictionary := mydict.Dictionary{"first": "First word"}
-	word := "hello"
-	definition := "Greeting"
-	
-	err := dictionary.Add(word, definition)
-	if err != nil {
-		fmt.Println(err)
-	}
-	hello, _ := dictionary.Search(word)
-	fmt.Println("found", word, "definition", hello) // found hello definitnio Greeting
-	
-	err2 := dictionary.Add(word, definition)
-	if err2 != nil {
-		fmt.Println(err2) // That word already exists
+	c :=make(chan string)
+	people := [5]string{"aaron", "jang", "jenny", "sam", "kate"}
+	for _, person := range people {
+		go isSexy(person, c) // goroutine has 2 arguments : person & channel
+	} // goroutine을 실행// channel을 통해서 결과를 받는것을 기다림
+	for i:=0; i <len(people); i++ {
+		// channel을 통해서 결과를 받는것을 기다림 (Receiving message through channel : blocking operation)
+		s := fmt.Sprintf("Received this message: %s", <-c)
+		fmt.Print("Waiting for ", i)
+		fmt.Println(s)
 	}
 
-	err3 := dictionary.Update("first", "Not First word")
-	if err3 != nil {
-		fmt.Println(err3)
-	}
-	err4 := dictionary.Delete("second")
-	if err4 != nil {
-		fmt.Println(err4) // Cant delete non-existing word
-	}
 }
+
+func isSexy(person string, channel chan string) {
+	time.Sleep(time.Second *5)
+	channel <- person + " is Sexy"
+}
+
+/*
+1. goroutine process ends when main function process ends
+2. you have to specify the type of data you are going to send&receive via channel
+3. sending message : channel <- message
+   receiving message : <- channel
+*/
